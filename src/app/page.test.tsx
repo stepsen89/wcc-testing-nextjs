91d57f4c55ddc939b62542a2aeab90e0c6a51d88
Steph <stepsen.dev@gmail.com>
2024-09-23 19:09:39 +0100

    update

---
+++ src/app/page.test.tsx
@@ -0,0 +1,20 @@
+// __tests__/Calculator.test.tsx
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Calculator from "./page";
+
+describe("Calculator", () => {
+  test("renders correctly with initial display of 0", () => {});
+
+  test("updates the display when numbers and operators are clicked", () => {});
+
+  test("invalid expression", () => {});
+
+  test("performs addition correctly", () => {});
+
+  test("performs division correctly", () => {});
+
+  test('shows "Infinity" when dividing through 0', () => {});
+
+  test("clears the display when C is clicked", () => {});
+});
